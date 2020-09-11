132ecede57593e627393a4f822a5afb424abc695
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-09-11 15:33:48 +0700

    add dates query

---
+++ Doccuments/Months.sql
@@ -0,0 +1,17 @@
+create table months(
+		id int identity(1,1) not null,
+		month int not null
+)
+
+insert [dbo].[months] ([month]) values (01)
+insert [dbo].[months] ([month]) values (02)
+insert [dbo].[months] ([month]) values (03)
+insert [dbo].[months] ([month]) values (04)
+insert [dbo].[months] ([month]) values (05)
+insert [dbo].[months] ([month]) values (06)
+insert [dbo].[months] ([month]) values (07)
+insert [dbo].[months] ([month]) values (08)
+insert [dbo].[months] ([month]) values (09)
+insert [dbo].[months] ([month]) values (10)
+insert [dbo].[months] ([month]) values (11)
+insert [dbo].[months] ([month]) values (12)
